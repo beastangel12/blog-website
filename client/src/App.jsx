b12e29364b2c5d9d663536403aeafe1e10627902
Angel Bista <anzilbista123@gmail.com>
2024-06-28 08:09:05 +0545

    pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Homes";
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 export default function App() {
-  return <h1 className="text-3xl text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,4 @@
-import React from 'react'
-
 export default function About() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <h1 className="text-3xl text-red-500 ">
+    Welcome To The Blog</h1>;
 }
