1542c9d5e183c96d2b4d63c7de1f922559967196
Angel Bista <anzilbista123@gmail.com>
2024-07-01 12:44:09 +0545

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,4 @@
+// controllers = means logic function inside it.
+export const test = (req, res) => {
+  res.json({ message: "API is working!" });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -18,3 +19,6 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+// we are use to get request from userRoutes so, we used use
+app.use("/api/user", userRoutes);
