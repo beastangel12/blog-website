4d218da0562a3f1b4eb8e42fed5fd2f6b8a20da1
Angel Bista <anzilbista123@gmail.com>
2024-07-07 10:38:30 +0545

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,7 @@
 // so, it should be synchronous because we want to create it.
 
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/User.model.js";
 import { errorhandler } from "../utils/error.js";
 
@@ -14,7 +15,7 @@ export const signup = async (req, res, next) => {
   if (
     !username ||
     !email ||
-    !password ||  
+    !password ||
     username === "" ||
     email === "" ||
     password === ""
@@ -37,6 +38,41 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json({ message: "signup successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorhandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorhandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorhandler(400, "Invalid password"));
+    }
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 // post router because we will gonna create something
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
