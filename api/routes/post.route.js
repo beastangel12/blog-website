c3d6d366498735dcb300342e76eba91e1c27d60c
Angel Bista <anzilbista123@gmail.com>
2024-07-19 12:27:28 +0545

    show user posts inside dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -5,6 +5,6 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
-router.get('/getposts', getposts)
+router.get("/getposts", getposts);
 
 export default router;
